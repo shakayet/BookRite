da625c4203ffe35c6b4cc42133379986e1d97cad
Shakayet <srabonshakhawat@gmail.com>
2025-06-29 02:27:53 +0600

    Update service route

--- src/app/modules/service/service.routes.ts
+++ src/app/modules/service/service.routes.ts
@@ -6,8 +6,17 @@ import { USER_ROLES } from '../../../enums/user';
 
 const router = express.Router();
 
-router.post('/', auth(USER_ROLES.PROVIDER), ServiceController.createService);
-router.get('/', ServiceController.getAllServices);
+
+router.route('/')
+    .post(
+       auth(USER_ROLES.PROVIDER),
+       ServiceController.createService 
+    )
+    .get(
+        ServiceController.getAllServices
+    );
+
+
 router.get('/categories', ServiceController.getAllCategories);
 router.get('/trending', ServiceController.getTrendingServices);
 // router.get('/recommendations', auth(), ServiceController.getRecommendedServices);
@@ -15,13 +24,21 @@ router.get('/review', ServiceController.getServiceReviews);
 router.get('/portfolio', auth(), ServiceController.getServicePortfolio);
 router.get('/:category', ServiceController.getServiceByCategory);
 router.get('/details/:id', ServiceController.getServiceDetails);
-router.delete('/:id', auth(USER_ROLES.ADMIN, USER_ROLES.PROVIDER), ServiceController.deleteService);
-router.patch('/:id', auth(USER_ROLES.PROVIDER), ServiceController.updateService);
 router.post('/book/:id', auth(USER_ROLES.USER), ServiceController.bookServiceSlot);
 router.get('/provider/dashboard', auth(USER_ROLES.PROVIDER), ServiceController.getProviderDashboard);
 router.patch('/update-booking/:serviceId/:bookingId',auth(USER_ROLES.PROVIDER),ServiceController.updateBookingStatus);
 router.patch('/rate-booking/:serviceId/:bookingId',auth(USER_ROLES.USER),ServiceController.rateBooking);
 router.get('/recommended/top', ServiceController.getTopRecommendedServices);
+
+router.route('/:id')
+    .patch(
+        auth(USER_ROLES.PROVIDER), 
+        ServiceController.updateService
+    )
+    .delete(
+        auth(USER_ROLES.ADMIN, USER_ROLES.PROVIDER), 
+        ServiceController.deleteService
+    );
 
 
 export const ServiceRoutes=router
